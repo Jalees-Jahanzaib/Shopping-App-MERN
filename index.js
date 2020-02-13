087650c445f0c65aeaee539fe0d99a43bc269b1d
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-02-13 11:59:14 +0530

    Mongodb Atlas Working

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express();
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://Jalees:jal123@cluster0-bwh1r.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true },
-    { useUnifiedTopology: true }
-).then(() => console.log('Mongoose Connected')).catch(err => console.error(err));
+const mongoose = require('mongoose')
+const db = 'mongodb+srv://Jahanzaib:zxcvbnm123@cluster0-bwh1r.mongodb.net/test?retryWrites=true&w=majority'
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('MongoDb connected;')
+
+    }
+    catch (err) {
+        console.error(err.message);
+        process.exit(0)
+    }
+}
+connectDB()
 app.get('/', (req, res) => {
     res.send("Hello! World")
 })
