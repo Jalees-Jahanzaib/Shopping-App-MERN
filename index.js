5043755d43d12a70937b2fc1eb50ff74e5af952f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-02-13 11:17:07 +0530

    Connected to MongoDB external

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://Jalees:jal123@cluster0-bwh1r.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+).then(() => console.log('Mongoose Connected')).catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send("Hello! World")
