f6be0f06289134cc03083fb63eb61cc3c779ebcf
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-02-13 13:28:17 +0530

    Postman not Working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
+const { User } = require('./models/user')
 const mongoose = require('mongoose')
 const db = 'mongodb+srv://Jahanzaib:zxcvbnm123@cluster0-bwh1r.mongodb.net/test?retryWrites=true&w=majority'
-
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
@@ -20,7 +22,17 @@ const connectDB = async () => {
     }
 }
 connectDB()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(cookieParser())
 app.get('/', (req, res) => {
     res.send("Hello! World")
+})
+app.post('api/users/register', async (req, res) => {
+    const user = new User(res.body)
+    await user.save()
+
+
 })
 app.listen(5000)
