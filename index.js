ac1adcba0a9945227f12131532f3daedce633a13
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-02-13 11:20:11 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://Jalees:jal123@cluster0-bwh1r.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
 ).then(() => console.log('Mongoose Connected')).catch(err => console.error(err));
 
 app.get('/', (req, res) => {
